ee554050fcbbb10fdbe73e89a0c29315ad80a308
Brunocarpi <brunocarpi385@gmail.com>
2024-10-15 09:10:38 -0300

    aula 15_10 criação do cadastro de filmes

--- front/js/script.js
+++ front/js/script.js
@@ -1,16 +1,29 @@
 const protocolo = 'http://'
 const baseURL = 'localhost:3000'
-const filmeEndpoint = '/filmes'
+const filmesEndpoint = '/filmes'
 
 async function obterFilmes() {
-    const URLcompleta = `${protocolo}${baseURL}${filmeEndpoint}`;
-    console.log(`Requisitando: ${URLcompleta}`); // Adicione este log
+    const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`;
+    console.log(`Requisitando: ${URLcompleta}`); 
+
+function listarFilmes (filmes) {
+    let tabela = document.querySelector('.filmes')
+    let corpoTabela = tabela.getElementsByTagName('tbody'[0])
+    corpoTabela.innerHTML=""
+    for (let filme of filmes) {
+        let linha = corpoTabela.insertRow(0);
+        let celulaTitulo = linha.insertCell(0);
+        let celulaSinopse = linha.insertCell(1);
+        celulaTitulo.innerHTML = filme.titulo;
+        celulaSinopse.innerHTML = filme.sinopse;
+    }
+}
 
     try {
         const response = await axios.get(URLcompleta);
-        console.log(response); // Adicione este log para ver a resposta completa
+        console.log(response); 
         const filmes = response.data;
-        console.log(filmes); // Adicione este log para ver os dados dos filmes
+        console.log(filmes); 
         
         let tabela = document.querySelector('.filmes');
         let corpoTabela = tabela.getElementsByTagName('tbody')[0];
@@ -22,6 +35,27 @@ async function obterFilmes() {
             celulaSinopse.innerHTML = filme.sinopse;
         }
     } catch (error) {
-        //console.error(`Erro ao obter filmes: ${error}`); // Adicione este log para capturar erros
+    }
+}
+async function cadastrarFilme() {
+    const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`;
+    let tituloInput = document.querySelector('#tituloInput')
+    let sinopseInput = document.querySelector('#sinopseInput')
+    let titulo = tituloInput.value
+    let sinopse = sinopseInput.value
+    tituloInput.value = ""
+    sinopseInput.value = ""
+    const filmes = (await axios.post(URLcompleta, {titulo, sinopse})).data
+    listarFilmes(filmes)
+
+    let tabela = document.querySelector('.filmes')
+    let corpoTabela = tabela.getElementsByTagName('tbody'[0])
+    corpoTabela.innerHTML=""
+    for (let filme of filmes) {
+        let linha = corpoTabela.insertRow(0);
+        let celulaTitulo = linha.insertCell(0);
+        let celulaSinopse = linha.insertCell(1);
+        celulaTitulo.innerHTML = filme.titulo;
+        celulaSinopse.innerHTML = filme.sinopse;
     }
 }
